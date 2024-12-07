7af33f1f2c2a7212b84125e49372f851792f59e8
gyver5681 <davemarker@gmail.com>
2024-12-06 20:17:19 -0500

    Fixes for Multiplayer

--- meta.ps1
+++ meta.ps1
@@ -1,6 +1,6 @@
 
-$SourceEntitiesFolder = "C:\Projects\MCAddons\sources\more mob heads v2.14.2 (MC 1.21-1.21.3)\data\more_mob_heads\loot_table\entities"
-$OutputProjectRoot = "C:\Projects\MCAddons\\MoreMobHeads"
+$SourceEntitiesFolder = "C:\Projects\MCAddons\stable\sources\more mob heads v2.14.2 (MC 1.21-1.21.3)\data\more_mob_heads\loot_table\entities"
+$OutputProjectRoot = "C:\Projects\MCAddons\stable\MoreMobHeads"
 $OutputLogFile = "$($OutputProjectRoot)\output.log"
 $BlockTemplateFile = "$($OutputProjectRoot)\blocks.txt"
 
@@ -15,7 +15,7 @@ $OutputRPFolder = "$($OutputProjectRoot)\\resource_packs\more-mob-heads"
 # Output Behavior Pack Folders
 $OutputBpBlocksFolder = "$($OutputBPFolder)\blocks"
 $OutputBpItemsFolder = "$($OutputBPFolder)\items"
-$OutputBpEntitiesLootTableFolder = "$($OutputBPFolder)\loot_tables\entities"
+$OutputBpBlocksLootTableFolder = "$($OutputBPFolder)\loot_tables\blocks"
 
 # Output Resource Pack Folders
 $OutputRpAttachablesFolder = "$($OutputRPFolder)\attachables"
@@ -38,10 +38,9 @@ $ReplaceLitItemName = "ITEM_NAME"
 
 
 # JSON Snippits
-$BlocksJsonHeader = "{`n  ""format_version"": ""$($FormatVersionBlocks)"",`n"
+$BlocksJsonHeader = "{`n  ""format_version"": ""$($FormatVersionBlocks)"","
 
-$TerrainTexturesHeader = "{`n ""resource_pack_name"": ""vanilla"",`n ""texture_name"": ""atlas.terrain"",`n ""padding"": 8,`n ""num_mip_levels"": 4,`n ""texture_data"": {`n"
-
+$TerrainTexturesHeader = "{`n  ""resource_pack_name"": ""vanilla"",`n  ""texture_name"": ""atlas.terrain"",`n  ""padding"": 8,`n  ""num_mip_levels"": 4,`n  ""texture_data"": {"
 
 function DownloadSkin {
   param(
@@ -69,19 +68,25 @@ function BuildItemSet {
   ## Resource Pack
   $ItemFileName = "$($CleanedItemName).png"
   Copy-Item -Path "$($OutputRpAttachableTexturesFolder)\$($ItemFileName)" -Destination "$($OutputRpBlockTexturesFolder)\$($ItemFileName)"
-  Add-Content -Path $OutputRpBlocks -Value """$($CleanedItemName)"": { ""sound"": ""stone"", ""textures"": ""$($CleanedItemName)"" },`n"
-  Add-Content -Path $OutputRpTerrainTextures -Value """$($CleanedItemName)"": { ""textures"": ""textures/blocks/$($CleanedItemName)"" },`n"
+  if ($CleanedItemName -eq "zombified_piglin_head") {
+    Add-Content -Path $OutputRpBlocks -Value "  ""$($CleanedItemName)_block"": { ""sound"": ""stone"", ""textures"": ""$($CleanedItemName)"" }"
+    Add-Content -Path $OutputRpTerrainTextures -Value "    ""$($CleanedItemName)"": { ""textures"": ""textures/blocks/$($CleanedItemName)"" }"
+  }
+  else {
+    Add-Content -Path $OutputRpBlocks -Value "  ""$($CleanedItemName)_block"": { ""sound"": ""stone"", ""textures"": ""$($CleanedItemName)"" },"
+    Add-Content -Path $OutputRpTerrainTextures -Value "    ""$($CleanedItemName)"": { ""textures"": ""textures/blocks/$($CleanedItemName)"" },"
+  }
   Add-Content -Path $OutputItemLangFile -Value "item.moremobheads:$($CleanedItemName)=$($ItemName)"
-  Add-Content -Path $OutputTileLangFile -Value "tile.moremobheads:$($CleanedItemName).name=$($ItemName) Block"
+  Add-Content -Path $OutputTileLangFile -Value "tile.moremobheads:$($CleanedItemName)_block.name=$($ItemName) Block"
   Set-Content -Path "$($OutputRpAttachablesFolder)\$($CleanedItemName).attachable.json" -Value "{`n  ""format_version"": ""$($FormatVersionAttachable)"",`n  ""minecraft:attachable"": {`n    ""description"": {`n      ""identifier"": ""moremobheads:$($CleanedItemName)"",`n      ""render_controllers"": [""controller.render.armor""],`n      ""materials"": {`n        ""default"": ""entity_alphatest"",`n        ""enchanted"": ""entity_alphatest_glint""`n      },`n      ""textures"": {`n        ""default"": ""textures/entity/attachable/$($CleanedItemName)"",`n        ""enchanted"": ""textures/misc/enchanted_item_glint""`n      },`n      ""geometry"": {`n        ""default"": ""geometry.more_mob_head""`n      }`n    }`n  }`n}"
+  Set-Content -Path "$($OutputBpBlocksLootTableFolder)\$($CleanedItemName).json" -Value "{`n  ""pools"": [{ ""rolls"": 1, ""entries"": [{ ""type"": ""item"", ""name"": ""moremobheads:$($CleanedItemName)"" }] }]`n}"
 
   ## Behavior Pack
   (Get-Content -Path $BlockTemplateFile) |
   ForEach-Object { ($_).replace($ReplaceLitBlockFormatVersion, $FormatVersionBlocks).replace($ReplaceLitItemName, $CleanedItemName) } |
   Set-Content -Path "$($OutputBpBlocksFolder)\$($CleanedItemName).json"
 
-  Set-Content -Path "$($OutputBpItemsFolder)\$($CleanedItemName).item.json" -Value "{`n  ""format_version"": ""$($FormatVersionItem)"",`n  ""minecraft:item"": {`n    ""description"": {`n      ""identifier"": ""moremobheads:$($CleanedItemName)"",`n      ""menu_category"": {`n        ""category"": ""items"",`n        ""group"": ""itemGroup.name.skull""`n      }`n    },`n    ""components"": {`n      ""minecraft:max_stack_size"": 1,`n      ""minecraft:wearable"": {`n        ""slot"": ""slot.armor.head"",`n        ""protection"": 0`n      },`n      ""minecraft:block_placer"": { ""block"": ""moremobheads:$($CleanedItemName)"" }`n    }`n  }`n}"
-
+  Set-Content -Path "$($OutputBpItemsFolder)\$($CleanedItemName).item.json" -Value "{`n  ""format_version"": ""$($FormatVersionItem)"",`n  ""minecraft:item"": {`n    ""description"": {`n      ""identifier"": ""moremobheads:$($CleanedItemName)"",`n      ""menu_category"": {`n        ""category"": ""items"",`n        ""group"": ""itemGroup.name.skull""`n      }`n    },`n    ""components"": {`n      ""minecraft:max_stack_size"": 1,`n      ""minecraft:wearable"": {`n        ""slot"": ""slot.armor.head"",`n        ""protection"": 0`n      },`n      ""minecraft:block_placer"": { ""block"": ""moremobheads:$($CleanedItemName)_block"" }`n    }`n  }`n}"
 
 }
 
@@ -150,7 +155,7 @@ ForEach-Object {
 }
 
 Add-Content -Path $OutputRpBlocks -Value "}"
-Add-Content -Path $OutputRpTerrainTextures -Value "}}"
+Add-Content -Path $OutputRpTerrainTextures -Value "  }`n}"
 
 Get-Content -Path $OutputItemLangFile | Set-Content -Path $OutputRpTexts
 Add-Content -Path $OutputRpTexts -Value "`n"
